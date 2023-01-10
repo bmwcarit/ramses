947e145f87757a34923e2fda46c3c83ba272723f
Julian Müller-Huschke <julian.mueller-huschke@paradoxcat.com>
2023-01-10 11:27:55 +0100

    fixed copyright notices

--- cmake/modules/FindZLIB.cmake
+++ cmake/modules/FindZLIB.cmake
@@ -1,5 +1,5 @@
 #  -------------------------------------------------------------------------
-#  Copyright (C) 2018 BMW Car IT GmbH
+#  Copyright (C) 2022 BMW AG
 #  -------------------------------------------------------------------------
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
